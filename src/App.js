00bb136f8527177f743d71f6159087604dc6a13f
Venturing Guy <LVentura335@Gmail.com>
2021-09-11 17:58:19 -0700

    basic functionality implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+
+const PIZZA_TOPPINGS = ['Pepperoni', 'Sausage', 'Peppers', 'Onions']
 
 function App() {
+  const [name, setName] = useState('')
+  const [toppings, setToppings] = useState([false, false, false, false]);
+  // Define list of toppings
+  // Make function to output individual inputs
+    // checkmark input type, name, checked or not?
+    // call function on check
+  const ToppingCheckbox = (props) => {
+    const {name, label, toppingChange, index} = props
+    return (
+        <label>
+            <input
+                type="checkbox"
+                name={name}
+                checked={toppings[index]}
+                onChange={toppingChange}
+            />
+        {label}</label>
+    )}
+  // Make function to render input component
+    // name and checkmark
+  const renderToppingCheckbox = (name, label, toppingChange, indexer) => (
+      <ToppingCheckbox
+          name={name}
+          label={label}
+          toppingChange={toppingChange}
+          index={indexer}
+      />
+  )
+  // Make function to render an input component for every topping defined in the list
+  const ToppingCheckboxes = PIZZA_TOPPINGS.map((_, index) => {
+      const name="topping"
+      const label=`${PIZZA_TOPPINGS[index]}`
+      const indexer=index
+      const toppingChange = () => changeToppings(index)
+      return renderToppingCheckbox(name, label, toppingChange, indexer)
+  })
+
+  const changeToppings = (index) => {
+    const toppingsCopy = [...toppings]
+    toppingsCopy[index] = !toppingsCopy[index]
+    setToppings(toppingsCopy)
+  }
+
+  const outputToppings=PIZZA_TOPPINGS.map((_, index) => {
+    return <p>{toppings[index] ? PIZZA_TOPPINGS[index] : null}</p>
+  })
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pizza Order</h1>
+      <input
+          placeholder="Name"
+          value={name}
+          onChange={e => setName(e.target.value)}
+      />
+      <div classname="toppingSelections">{ToppingCheckboxes}</div>
+      <button type="submit">Submit</button>
+      <h1>Your Order</h1>
+      <h2>{name}</h2>
+      <div className="toppingOutput">{outputToppings}</div>
     </div>
   );
 }
